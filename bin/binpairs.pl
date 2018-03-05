672e5d488faefaf750fce1141a86c5ecf05398df
Michael Schatz <michael.schatz@gmail.com>
2018-03-05 15:27:48 -0500

    add

---
+++ bin/binpairs.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl 
+use strict;
+
+my $USAGE = "binpairs.pl binsize < pairs > bins.txt\n";
+
+## plot with this in R
+## t<-read.table("scaffolds85.b100k.txt")
+## m<-as.matrix(t)
+## image(m)
+
+my $binsize = shift @ARGV or die $USAGE;
+
+my %counts;
+
+my $maxbin = 0;
+
+while (<>)
+{
+  chomp;
+  my ($s1, $s2) = split /\s+/, $_;
+
+  my $b1 = int ($s1 / $binsize);
+  my $b2 = int ($s2 / $binsize);
+
+  #print "$s1 $s2 | $b1 $b2\n";
+
+  $counts{$b1}->{$b2}++;
+  $counts{$b2}->{$b1}++;
+
+  if ($b1 > $maxbin) { $maxbin = $b1; }
+  if ($b2 > $maxbin) { $maxbin = $b2; }
+}
+
+for (my $b1 = 0; $b1 <= $maxbin; $b1++)
+{
+  for (my $b2 = 0; $b2 <= $maxbin; $b2++)
+  {
+    if ($b2 > 0) { print "\t"; }
+
+    my $c = 0;
+
+    if (exists $counts{$b1}->{$b2})
+    {
+      $c = $counts{$b1}->{$b2};
+    }
+
+    print $c;
+  }
+
+  print "\n";
+}
